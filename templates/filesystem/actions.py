e9e79b104ba96aec8163c38c3455b9ad3f35e894
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-03-23 11:10:39 +0100

    stub for filesystem service
    realtes to https://github.com/g8os/grid/issues/11

---
+++ templates/filesystem/actions.py
@@ -0,0 +1,18 @@
+
+def install(job):
+    raise NotImplementedError()
+
+def delete(job):
+    raise NotImplementedError()
+
+def snaptshot(job):
+    raise NotImplementedError()
+
+def rollback(job):
+    raise NotImplementedError()
+
+def mount(job):
+    raise NotImplementedError()
+
+def umount(job):
+    raise NotImplementedError()
---
+++ templates/filesystem/schema.capnp
@@ -0,0 +1,9 @@
+@0xfe38816f8b1419d9;
+
+struct Schema {
+    storagePool @0 :Text; # Name of the parent
+    sizeOnDisk @1 :Uint64; # Amount of MiB of storage used by the filesystem
+    readOnly @2: Bool;
+    quota @3: Uint64; # Amount of MiB that can be written to the filesystem. 0 means no quota is set.
+    mountpoint @4:Text;
+}
