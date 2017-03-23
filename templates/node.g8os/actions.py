dd22b64277d3df08c094f7d9d717c8736d92b51c
Geert Audenaert <geert@rogerthat.net>
2017-03-23 17:26:11 +0100

    Update actions.py

--- templates/node.g8os/actions.py
+++ templates/node.g8os/actions.py
@@ -2,6 +2,7 @@
 def install(job):
     raise NotImplementedError()
 
+    # at each boot recreate the complete state in the system
     # configure network
     # create storage pool for fuse cache
 
