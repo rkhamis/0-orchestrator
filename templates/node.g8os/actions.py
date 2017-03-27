f39ab0b353ae095a6b4f6134537598d291bc43c5
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-03-27 10:07:42 +0200

    node.g8os: installation of the storagepool used for fs cache

--- templates/node.g8os/actions.py
+++ templates/node.g8os/actions.py
@@ -1,10 +1,30 @@
 
 def install(job):
-    raise NotImplementedError()
-
     # at each boot recreate the complete state in the system
-    # configure network
+    service = job.service
+    node = j.sal.g8os.get_node(
+        addr=service.model.data.redisAddr,
+        port=service.model.data.redisPort,
+        password=service.model.data.redisPassword or None,
+    )
+
     # create storage pool for fuse cache
+    storagepool = node.ensure_persistance()
+    actor_storagepool = service.aysrepo.actorGet('storagepool')
+    sp_args = {
+        'status':'healthy',
+        'totalCapacity':storagepool.size,
+        'freeCapacity': storagepool.size - storagepool.used,
+        # 'metadataProfile': storagepool.metadata_profile, #TODO: fix g8os sal to have btrfs profile in object
+        # 'dataProfile': storagepool.data_profile,
+        'mountpoint': storagepool.mountpoint,
+        'devices': storagepool.devices,
+        'node': service.name,
+    }
+    actor_storagepool.serviceCreate(instance='{}_{}'.format(service.name, storagepool.name), args=sp_args)
+
+    # configure network
+    # TODO
 
 
 def reboot(job):
