8fb6f2485d62b7d98ce22b0f346478d923c12f1a
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-03-28 17:59:07 +0200

    specs for volume actor template

---
+++ templates/volume/actions.py
@@ -0,0 +1,17 @@
+
+def install(job):
+    raise NotImplementedError()
+    # start a new nbd server for this volume
+
+def start(job):
+    raise NotImplementedError()
+
+def pause(job):
+    raise NotImplementedError()
+
+def rollback(job):
+    raise NotImplementedError()
+
+def resize(job):
+    raise NotImplementedError()
+    # can only grow
