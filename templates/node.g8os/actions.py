7dce44277fca2a6ebf8f8c9afee450f4b16555f7
Jo De Boeck <deboeck.jo@gmail.com>
2017-03-28 14:01:57 +0200

    Add initial implementation for storagepool (#33)
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- templates/node.g8os/actions.py
+++ templates/node.g8os/actions.py
@@ -1,3 +1,5 @@
+from JumpScale import j
+
 
 def install(job):
     # at each boot recreate the complete state in the system
@@ -8,23 +10,30 @@ def install(job):
         password=service.model.data.redisPassword or None,
     )
     # create storage pool for fuse cache
-    storagepool = node.ensure_persistance()
+    poolname = "{}_fsache".format(service.name)
+    storagepool = node.ensure_persistance(poolname)
     actor_storagepool = service.aysrepo.actorGet('storagepool')
     sp_args = {
-        'status':'healthy',
-        'totalCapacity':storagepool.size,
+        'status': 'healthy',
+        'totalCapacity': storagepool.size,
         'freeCapacity': storagepool.size - storagepool.used,
-        # 'metadataProfile': storagepool.metadata_profile, #TODO: fix g8os sal to have btrfs profile in object
-        # 'dataProfile': storagepool.data_profile,
+        'metadataProfile': storagepool.fsinfo['metadata']['profile'],
+        'dataProfile': storagepool.fsinfo['data']['profile'],
         'mountpoint': storagepool.mountpoint,
         'devices': storagepool.devices,
         'node': service.name,
     }
-    actor_storagepool.serviceCreate(instance='{}_{}'.format(service.name, storagepool.name), args=sp_args)
+    actor_storagepool.serviceCreate(instance=poolname, args=sp_args)
 
     # configure network
     # TODO
 
 
 def reboot(job):
-    raise NotImplementedError()
+    service = job.service
+    node = j.sal.g8os.get_node(
+        addr=service.model.data.redisAddr,
+        port=service.model.data.redisPort,
+        password=service.model.data.redisPassword or None,
+    )
+    node.client.raw('core.reboot', {})
--- templates/storagepool/actions.py
+++ templates/storagepool/actions.py
@@ -1,13 +1,65 @@
+from JumpScaler import j
+
+
+def input(job):
+    service = job.service
+    for notempty in ['mountpoint', 'metadataProfile', 'dataProfile', 'devices']:
+        if job.model.args.get(notempty, "") == "":
+            raise j.exceptions.Input("{} argument cannot be empty, cannot continue init of {}".format(notempty, service))
+
 
 def install(job):
-    # Mount and or create (if it does not exist yet on the g8os) the btrfs filesystem.
-    raise NotImplementedError()
+    service = job.service
+    pservice = service.parent
+    node = j.sal.g8os.get_node(
+        addr=pservice.model.data.redisAddr,
+        port=pservice.model.data.redisPort,
+        password=pservice.model.data.redisPassword or None,
+    )
+    devices = list(service.model.data.devices)
+    name = service.name
+    dataProfile = str(service.model.data.dataProfile)
+    metadataProfile = str(service.model.data.metadataProfile)
+    mountpoint = str(service.model.data.mountpoint)
+    try:
+        pool = node.storagepools.get(name)
+    except ValueError:
+        # pool does not exists lets create it
+        pool = node.storagepools.create(name, devices, metadataProfile, dataProfile)
+
+    # mount device
+    if pool.mountpoint:
+        if pool.mountpoint != mountpoint:
+            pool.umount(mountpoint)
+            pool.mount(mountpoint)
+    else:
+        pool.mount(mountpoint)
+
+    # lets check if devices need to be added removed and the profile still matches
+    if pool.fsinfo['data']['profile'].lower() != dataProfile:
+        raise RuntimeError("Data profile of storagepool {} does not match".format(name))
+    if pool.fsinfo['metadata']['profile'].lower() != metadataProfile:
+        raise RuntimeError("Metadata profile of storagepool {} does not match".format(name))
+
+    pooldevices = set(pool.devices)
+    requireddevices = set(devices)
+
+    # add extra devices
+    extradevices = requireddevices - pooldevices
+    pool.device_add(*extradevices)
+
+    # remove devices
+    removeddevices = pooldevices - requireddevices
+    pool.device_remove(*removeddevices)
+
 
 def delete(job):
-    raise NotImplementedError()
-
-def addDevice(job):
-    raise NotImplementedError()
-
-def removeDevice(job):
-    raise NotImplementedError()
+    service = job.service
+    pservice = service.parent
+    node = j.sal.g8os.get_node(
+        addr=pservice.model.data.redisAddr,
+        port=pservice.model.data.redisPort,
+        password=pservice.model.data.redisPassword or None,
+    )
+    name = service.name
+    node.storagepools.destroy(name)
