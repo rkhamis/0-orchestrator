8bf0d123941406491d996c2856a29b050550da5f
Sarah Boctor <boctor.sarah@gmail.com>
2017-03-29 16:56:38 +0200

    Implement GetNodeOsInfo

--- node_api_GetNodeOSInfo.go
+++ node_api_GetNodeOSInfo.go
@@ -3,11 +3,33 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"github.com/g8os/go-client"
 )
 
 // GetNodeOSInfo is the handler for GET /node/{nodeid}/info
 // Get detailed information of the os of the node
 func (api NodeAPI) GetNodeOSInfo(w http.ResponseWriter, r *http.Request) {
-	var respBody OSInfo
+	cl := GetConnection(r)
+	info := client.Info(cl)
+	os, err := info.OS()
+
+	if err != nil {
+		json.NewEncoder(w).Encode(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	respBody := OSInfo{
+		BootTime: int64(os.BootTime),
+		Hostname: os.Hostname,
+		Os: os.OS,
+		Platform: os.Platform,
+		PlatformFamily: os.PlatformFamily,
+		PlatformVersion: os.PlatformVersion,
+		Procs: int64(os.Procs),
+		Uptime: int64(os.Uptime),
+		VirtualizationRole: os.VirtualizationRole,
+		VirtualizationSystem: os.VirtualizationSystem,
+	}
 	json.NewEncoder(w).Encode(&respBody)
 }
