4e88d8719d6ec7635b50f3d720ae89f10f863d7b
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-03-29 14:16:08 +0200

    bootstrap.g8os: support when a service node service already exists

--- templates/bootstrap.g8os/actions.py
+++ templates/bootstrap.g8os/actions.py
@@ -1,3 +1,4 @@
+from JumpScale import j
 
 def bootstrap(job):
     # discover the node that send the event
@@ -9,19 +10,28 @@ def bootstrap(job):
     mac = j.sal.nettools.getMacAddressForIp(ip)
 
     service = job.service
-    # create and install the node.g8os service
-    node_actor = job.service.aysrepo.actorGet('node.g8os')
-    networks = [n.name for n in service.producers.get('network', [])]
 
-    node_args = {
-        'id': mac,
-        'status':'running',
+    try:
+        job.logger.info("service for node {} already exists, updating model".format(mac))
+        node = service.aysrepo.serviceGet(role='node', instance=mac)
+        # mac sure the service has the correct ip in his model.
+        # it could happend that a node get a new ip after a reboot
+        node.model.data.redisAddr = ip
+        node.model.data.status = 'running'
 
-        'networks': networks,
-        'redisAddr': ip,
-    }
-    job.logger.info("create node.g8os service {}".format(mac))
-    node = node_actor.serviceCreate(instance=mac, args=node_args)
+    except j.exceptions.NotFound:
+        # create and install the node.g8os service
+        node_actor = job.service.aysrepo.actorGet('node.g8os')
+        networks = [n.name for n in service.producers.get('network', [])]
 
-    job.logger.info("install node.g8os service {}".format(mac))
-    j.tools.async.wrappers.sync(node.executeActionJob('install'))
+        node_args = {
+            'id': mac,
+            'status':'running',
+            'networks': networks,
+            'redisAddr': ip,
+        }
+        job.logger.info("create node.g8os service {}".format(mac))
+        node = node_actor.serviceCreate(instance=mac, args=node_args)
+
+        job.logger.info("install node.g8os service {}".format(mac))
+        j.tools.async.wrappers.sync(node.executeActionJob('install'))
