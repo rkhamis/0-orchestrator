2b278063621e3567c2dd1d4ead5bdae0fbe37589
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-04-04 22:22:24 +0200

    implement deleteStoragepool method

--- api/node/node_api_DeleteStoragePool.go
+++ api/node/node_api_DeleteStoragePool.go
@@ -2,9 +2,24 @@ package node
 
 import (
 	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/g8os/grid/api/tools"
+	"github.com/gorilla/mux"
 )
 
 // DeleteStoragePool is the handler for DELETE /node/{nodeid}/storagepool/{storagepoolname}
 // Delete the storage pool
 func (api NodeAPI) DeleteStoragePool(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	name := vars["storagepoolname"]
+
+	_, err := api.AysAPI.Ays.DeleteServiceByName(name, "storagepool", api.AysRepo, nil, nil)
+	if err != nil {
+		log.Errorf("Error deleting storagepool services : %+v", err)
+		tools.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
