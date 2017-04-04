d1e2860dd8c18bedf2a6c69b95719ffcd6b2aee6
Khaled K. Badr <khaledkbadr@outlook.com>
2017-04-04 12:54:24 +0200

    Implement bridges api (GET)

--- node_api_ListBridges.go
+++ node_api_ListBridges.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	client "github.com/g8os/grid/ays-client"
 )
 
 // ListBridges is the handler for GET /node/{nodeid}/bridge
@@ -10,4 +12,14 @@ import (
 func (api NodeAPI) ListBridges(w http.ResponseWriter, r *http.Request) {
 	var respBody []Bridge
 	json.NewEncoder(w).Encode(&respBody)
+	cl := client.NewAtYourServiceAPI()
+	_, resp, err := cl.Ays.ListServicesByRole("bridge", AysRepo, nil, nil)
+	if err != nil {
+		WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		w.WriteHeader(resp.StatusCode)
+		return
+	}
 }
