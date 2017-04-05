82cda4867b609a86d02612f9f5fe8fafa61ab51e
Khaled K. Badr <khaledkbadr@outlook.com>
2017-04-05 16:00:59 +0200

    Implement getbridge

--- api/node/node_api_GetBridge.go
+++ api/node/node_api_GetBridge.go
@@ -3,6 +3,13 @@ package node
 import (
 	"encoding/json"
 	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
+
+	"fmt"
+
+	"github.com/g8os/grid/api/tools"
 )
 
 // GetBridge is the handler for GET /node/{nodeid}/bridge/{bridgeid}
@@ -10,4 +17,27 @@ import (
 func (api NodeAPI) GetBridge(w http.ResponseWriter, r *http.Request) {
 	var respBody Bridge
 	json.NewEncoder(w).Encode(&respBody)
+
+	vars := mux.Vars(r)
+	bridge := vars["bridgeid"]
+	srv, _, err := api.AysAPI.Ays.GetServiceByName(bridge, "bridge", api.AysRepo, nil, nil)
+	if err != nil {
+		log.Errorf("Error in getting bridge service %s : %+v", bridge, err)
+		tools.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err := json.Unmarshal(srv.Data, &respBody); err != nil {
+		tools.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	respBody.Name = srv.Name
+	fmt.Println(respBody)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(&respBody); err != nil {
+		log.Errorf("Error in encoding response: %+v", err)
+		tools.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
 }
