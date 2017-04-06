ac0a54430efd9a6b575249648907e0659086b4ae
Geert Audenaert <geert@mobicage.com>
2017-04-06 14:45:13 +0200

    Added utility method for handling AYS responses

--- api/tools/http.go
+++ api/tools/http.go
@@ -34,3 +34,17 @@ func NewHTTPError(resp *http.Response, msg string, args ...interface{}) HTTPErro
 func (httpErr HTTPError) Error() string {
 	return httpErr.err.Error()
 }
+
+func HandleAYSResponse(aysErr error, aysRes *http.Response, w http.ResponseWriter, action string) bool {
+	if aysErr != nil {
+		log.Errorf("AYS threw error while %s: %+v.\n", action, aysErr)
+		WriteError(w, http.StatusInternalServerError, aysErr)
+		return false
+	}
+	if aysRes.StatusCode != http.StatusOK {
+		log.Errorf("AYS returned status %v while %s.\n", aysRes.StatusCode, action)
+		w.WriteHeader(aysRes.StatusCode)
+		return false
+	}
+	return true
+}
