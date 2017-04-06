5a86de3db0b5b631888458bd5a2e84ca7ccdfa09
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-04-06 19:17:07 +0200

    use listing of service with fields for all listing methods

--- api/node/node_api_ListBridges.go
+++ api/node/node_api_ListBridges.go
@@ -2,15 +2,12 @@ package node
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
-	"sync"
 
 	log "github.com/Sirupsen/logrus"
-	"github.com/gorilla/mux"
-
-	"fmt"
-
 	"github.com/g8os/grid/api/tools"
+	"github.com/gorilla/mux"
 )
 
 // ListBridges is the handler for GET /nodes/{nodeid}/bridges
@@ -19,54 +16,31 @@ func (api NodeAPI) ListBridges(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	nodeid := vars["nodeid"]
 
-	queryParams := map[string]interface{}{"parent": fmt.Sprintf("node.g8os!%s", nodeid)}
+	queryParams := map[string]interface{}{
+		"parent": fmt.Sprintf("node.g8os!%s", nodeid),
+		"fields": "setting,status",
+	}
 	services, resp, err := api.AysAPI.Ays.ListServicesByRole("bridge", api.AysRepo, nil, queryParams)
-
-	if err != nil {
-		tools.WriteError(w, http.StatusInternalServerError, err)
-		return
-	}
-	if resp.StatusCode != http.StatusOK {
-		w.WriteHeader(resp.StatusCode)
+	if !tools.HandleAYSResponse(err, resp, w, "listing bridges") {
 		return
 	}
 
-	wg := sync.WaitGroup{}
-	var respBody = make([]Bridge, 0, len(services))
-	wg.Add(len(services))
+	var respBody = make([]Bridge, len(services))
+	for i, service := range services {
+		bridge := Bridge{
+			Name: service.Name,
+		}
 
-	for _, service := range services {
-		go func(name, role string) {
-			defer wg.Done()
+		if err := json.Unmarshal(service.Data, &bridge); err != nil {
+			log.Errorf("Error in listing bridges: %+v\n", err)
+			tools.WriteError(w, http.StatusInternalServerError, err)
+			return
+		}
 
-			srv, resp, err := api.AysAPI.Ays.GetServiceByName(name, role, api.AysRepo, nil, nil)
-			if err != nil {
-				log.Errorf("Error in listing bridges: %+v\n", err)
-				return
-			}
-
-			if resp.StatusCode != http.StatusOK {
-				log.Errorf("Error in listing bridges: %+v\n", err)
-				return
-			}
-
-			var bridge Bridge
-			if err := json.Unmarshal(srv.Data, &bridge); err != nil {
-				log.Errorf("Error in listing bridges: %+v\n", err)
-				return
-			}
-			bridge.Name = srv.Name
-
-			respBody = append(respBody, bridge)
-		}(service.Name, service.Role)
+		respBody[i] = bridge
 	}
-
-	wg.Wait()
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	if respBody == nil {
-		respBody = []Bridge{}
-	}
 	json.NewEncoder(w).Encode(&respBody)
 }
