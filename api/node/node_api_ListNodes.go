d763c76a3f7f67b7f6f0aa72d2b9a8add89370f9
Sarah Boctor <boctor.sarah@gmail.com>
2017-04-06 10:49:49 +0200

    Extend nodes api: - Implement ListNodes - Implement GetNode

--- api/node/node_api_ListNodes.go
+++ api/node/node_api_ListNodes.go
@@ -3,11 +3,48 @@ package node
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/g8os/grid/api/tools"
 )
 
-// ListNodes is the handler for GET /node
+// ListNodes is the handler for GET /nodes
 // List Nodes
 func (api NodeAPI) ListNodes(w http.ResponseWriter, r *http.Request) {
+	services, res, err := api.AysAPI.Ays.ListServicesByRole("node", api.AysRepo, nil, nil)
+	if err != nil {
+		tools.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	if res.StatusCode != http.StatusOK {
+		w.WriteHeader(res.StatusCode)
+		return
+	}
+
 	var respBody []Node
+	for _, service := range services {
+		srv, res, err := api.AysAPI.Ays.GetServiceByName(service.Name, "node", api.AysRepo, nil, nil)
+
+		if err != nil {
+			tools.WriteError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		if res.StatusCode != http.StatusOK {
+			w.WriteHeader(res.StatusCode)
+			return
+		}
+
+		var node Node
+		if err := json.Unmarshal(srv.Data, &node); err != nil {
+			tools.WriteError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		node.Id = service.Name
+		respBody = append(respBody, node)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(&respBody)
 }
