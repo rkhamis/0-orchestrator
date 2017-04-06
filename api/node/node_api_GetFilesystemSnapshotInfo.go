a69b5c8c91f4bf7348ab916913ec9b4de32a7358
Geert Audenaert <geert@mobicage.com>
2017-04-06 21:56:31 +0200

    Added GetFilesystemSnapshotInfo

--- api/node/node_api_GetFilesystemSnapshotInfo.go
+++ api/node/node_api_GetFilesystemSnapshotInfo.go
@@ -3,11 +3,31 @@ package node
 import (
 	"encoding/json"
 	"net/http"
+	"fmt"
+
+	"github.com/gorilla/mux"
+
+	tools "github.com/g8os/grid/api/tools"
 )
 
 // GetFilesystemSnapshotInfo is the handler for GET /nodes/{nodeid}/storagepools/{storagepoolname}/filesystem/{filesystemname}/snapshot/{snapshotname}
 // Get detailed information on the snapshot
 func (api NodeAPI) GetFilesystemSnapshotInfo(w http.ResponseWriter, r *http.Request) {
+	snapshotname := mux.Vars(r)["snapshotname"]
+
+	service, resp, err := api.AysAPI.Ays.GetServiceByName(snapshotname, "fssnapshot", api.AysRepo, nil, nil)
+	if ! tools.HandleAYSResponse(err, resp, w, fmt.Sprintf("getting snapshot %s details", snapshotname)) {
+		return
+	}
+
 	var respBody Snapshot
+
+	respBody.Name = snapshotname
+	if err := json.Unmarshal(service.Data, &respBody); err != nil {
+		tools.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(&respBody)
 }
--- templates/fssnapshot/schema.capnp
+++ templates/fssnapshot/schema.capnp
@@ -2,6 +2,8 @@
 
 struct Schema {
     filesystem @0 :Text; # Name of the parent
-    name @1 :Text; # Name of the snapshot
-    path @2 :Text;
+    name @1 : Text; # Name of the snapshot
+    path @2 : Text;
+    sizeOnDisk @3 : UInt32; # Amount of MiB of storage used by the filesystem
+    timestamp @4 : UInt32; # Creation timestamp
 }
