1901d1d5845eebb8e7df9cd0dcdcee81bcf27442
Sarah Boctor <boctor.sarah@gmail.com>
2017-04-06 12:48:47 +0200

    Implement ListContainers

--- api/node/node_api_ListContainers.go
+++ api/node/node_api_ListContainers.go
@@ -3,11 +3,67 @@ package node
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
+
+	"github.com/g8os/grid/api/tools"
+	"github.com/gorilla/mux"
 )
 
-// ListContainers is the handler for GET /node/{nodeid}/container
+// ListContainers is the handler for GET /nodes/{nodeid}/containers
 // List running Containers
 func (api NodeAPI) ListContainers(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	nodeID := vars["nodeid"]
+
+	query := make(map[string]interface{})
+	query["parent"] = strings.Join([]string{"node.g8os", nodeID}, "!")
+
+	services, res, err := api.AysAPI.Ays.ListServicesByRole("container", api.AysRepo, nil, query)
+	if err != nil {
+		tools.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+	if res.StatusCode != http.StatusOK {
+		w.WriteHeader(res.StatusCode)
+		return
+	}
+
+	type containerItem struct {
+		Flist    string                      `json:"flist" validate:"nonzero"`
+		Hostname string                      `json:"hostname" validate:"nonzero"`
+		Status   EnumContainerListItemStatus `json:"status" validate:"nonzero"`
+	}
+
 	var respBody []ContainerListItem
+	for _, service := range services {
+		srv, res, err := api.AysAPI.Ays.GetServiceByName(service.Name, "container", api.AysRepo, nil, nil)
+
+		if err != nil {
+			tools.WriteError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		if res.StatusCode != http.StatusOK {
+			w.WriteHeader(res.StatusCode)
+			return
+		}
+
+		var containerItem containerItem
+		if err := json.Unmarshal(srv.Data, &containerItem); err != nil {
+			tools.WriteError(w, http.StatusInternalServerError, err)
+			return
+		}
+
+		container := ContainerListItem{
+			Id:       service.Name,
+			Flist:    containerItem.Flist,
+			Hostname: containerItem.Hostname,
+			Status:   containerItem.Status,
+		}
+		respBody = append(respBody, container)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(&respBody)
 }
