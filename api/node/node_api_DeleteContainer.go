e44b8d63394be3a3950026c41e3be314766846c7
Ruben Mattan <ruben@mobicage.com>
2017-04-07 16:26:22 +0200

    Delete actor before deleting container service closes #57

--- api/node/node_api_DeleteContainer.go
+++ api/node/node_api_DeleteContainer.go
@@ -1,17 +1,51 @@
 package node
 
 import (
+	"fmt"
+	"net/http"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
 	"github.com/g8os/grid/api/tools"
 	"github.com/gorilla/mux"
-	"net/http"
 )
 
 // DeleteContainer is the handler for DELETE /nodes/{nodeid}/containers/{containerid}
 // Delete Container instance
 func (api NodeAPI) DeleteContainer(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	id := vars["containerid"]
-	res, err := api.AysAPI.Ays.DeleteServiceByName(id, "container", api.AysRepo, nil, nil)
+	containerID := vars["containerid"]
+
+	// execute the delete action of the snapshot
+	bp := map[string]interface{}{
+		"actions": []map[string]string{{
+			"action":  "delete",
+			"actor":   "container",
+			"service": containerID,
+		}},
+	}
+	bpName := fmt.Sprintf("container__%s_delete_%d", containerID, time.Now().Unix())
+
+	run, err := tools.ExecuteBlueprint(api.AysRepo, bpName, bp)
+	if err != nil {
+		httpErr := err.(tools.HTTPError)
+		log.Errorf("Error executing blueprint for container deletion : %+v", err.Error())
+		tools.WriteError(w, httpErr.Resp.StatusCode, httpErr)
+		return
+	}
+
+	// Wait for the delete job to be finshed before we delete the service
+	if err = tools.WaitRunDone(run.Key, api.AysRepo); err != nil {
+		httpErr, ok := err.(tools.HTTPError)
+		if ok {
+			tools.WriteError(w, httpErr.Resp.StatusCode, httpErr)
+		} else {
+			tools.WriteError(w, http.StatusInternalServerError, err)
+		}
+		return
+	}
+
+	res, err := api.AysAPI.Ays.DeleteServiceByName(containerID, "container", api.AysRepo, nil, nil)
 	if !tools.HandleAYSResponse(err, res, w, "deleting service") {
 		return
 	}
