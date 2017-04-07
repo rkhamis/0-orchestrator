35adf8aeab89c4c9fe366486005c57deb6f77d34
Ruben Mattan <ruben@mobicage.com>
2017-04-07 11:32:27 +0200

    Implemented GetZerotier

--- api/node/node_api_GetZerotier.go
+++ api/node/node_api_GetZerotier.go
@@ -2,12 +2,32 @@ package node
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/g8os/grid/api/tools"
+	"github.com/gorilla/mux"
 )
 
-// GetZerotier is the handler for GET /nodes/{nodeid}/zerotier/{zerotierid}
+// GetZerotier is the handler for GET /nodes/{nodeid}/zerotiers/{zerotierid}
 // Get Zerotier network details
 func (api NodeAPI) GetZerotier(w http.ResponseWriter, r *http.Request) {
 	var respBody Zerotier
+
+	vars := mux.Vars(r)
+	zerotierID := vars["zerotierid"]
+
+	srv, res, err := api.AysAPI.Ays.GetServiceByName(zerotierID, "zerotier", api.AysRepo, nil, nil)
+	if !tools.HandleAYSResponse(err, res, w, fmt.Sprintf("getting zerotier %s details", zerotierID)) {
+		return
+	}
+
+	if err := json.Unmarshal(srv.Data, &respBody); err != nil {
+		tools.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(&respBody)
 }
