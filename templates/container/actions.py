d95f90d9ca56a5b9803912136b87bb35df55e527
Jo De Boeck <deboeck.jo@gmail.com>
2017-04-10 15:42:25 +0200

    Fixes for creating container
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- templates/container/actions.py
+++ templates/container/actions.py
@@ -1,3 +1,6 @@
+from JumpScale import j
+
+
 def input(job):
     # make sure we always consume all the filesystems used in the mounts property
     args = job.model.args
@@ -14,6 +17,8 @@ def input(job):
 
 def install(job):
     job.service.model.data.status = "halted"
+    j.tools.async.wrappers.sync(job.service.executeAction('start'))
+
 
 def start(job):
     from JumpScale.sal.g8os.Container import Container
@@ -21,7 +26,7 @@ def start(job):
     container = Container.from_ays(job.service)
     container.start()
 
-    running, _ = container.is_running()
+    running = container.is_running()
     if running:
         job.service.model.data.status = "running"
     else:
@@ -46,11 +51,10 @@ def monitor(job):
     from JumpScale.sal.g8os.Container import Container
 
     container = Container.from_ays(job.service)
-    container.stop()
 
     if service.model.actionsState['install'] == 'ok':
-        running, process = container.is_running()
-        if not running:
+        running = container.is_running()
+        if not running and service.model.data.status == 'running':
             try:
                 job.logger.warning("container {} not running, trying to restart".format(service.name))
                 service.model.dbobj.state = 'error'
@@ -60,4 +64,15 @@ def monitor(job):
                     service.model.dbobj.state = 'ok'
             except:
                 job.logger.error("can't restart container {} not running".format(service.name))
+                service.model.dbobj.state = 'error'
+        elif running and service.model.data.status == 'halted':
+            try:
+                job.logger.warning("container {} running, trying to stop".format(service.name))
+                service.model.dbobj.state = 'error'
+                container.stop()
+                running, _ = container.is_running()
+                if not running:
+                    service.model.dbobj.state = 'ok'
+            except:
+                job.logger.error("can't stop container {} is running".format(service.name))
                 service.model.dbobj.state = 'error'
