eae196c03b7c04294d1f537fff25e1673a0bcf53
Abdulrahman tarek khalifa <Abdulrahman.t.khalifa@gmail.com>
2017-04-11 18:29:11 +0200

    rename storage cluster to storageclusters

--- api/storagecluster/storageclusters_api.go
+++ api/storagecluster/storageclusters_api.go
@@ -1,5 +1,16 @@
 package storagecluster
 
-// StorageclustersAPI is API implementation of /storageclusters root endpoint
+import ays "github.com/g8os/grid/api/ays-client"
+
+// StorageclusterAPI is API implementation of /storagecluster root endpoint
 type StorageclustersAPI struct {
+	AysRepo string
+	AysAPI  *ays.AtYourServiceAPI
+}
+
+func NewStorageClusterAPI(repo string, client *ays.AtYourServiceAPI) StorageclustersAPI {
+	return StorageclustersAPI{
+		AysRepo: repo,
+		AysAPI:  client,
+	}
 }
