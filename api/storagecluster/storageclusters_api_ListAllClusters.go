6e658749f654b7c303daed8b960e58a1f14a7dcb
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2017-04-13 14:11:21 +0200

    ensure that listStorageCluster returns an empty list and not null

--- api/storagecluster/storageclusters_api_ListAllClusters.go
+++ api/storagecluster/storageclusters_api_ListAllClusters.go
@@ -11,7 +11,7 @@ import (
 // ListAllClusters is the handler for GET /storageclusters
 // List all running clusters
 func (api StorageclustersAPI) ListAllClusters(w http.ResponseWriter, r *http.Request) {
-	var respBody []string
+	respBody := []string{}
 	type data struct {
 		Label string `json:"label"`
 	}
