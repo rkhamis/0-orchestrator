344a5321623b837653bdc8856353d9d7ca97a990
Jo De Boeck <deboeck.jo@gmail.com>
2017-04-19 17:48:10 +0200

    Add buidlscript for cloud-init-server
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

---
+++ buildscripts/builder-cloud-init-server.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+branch="master"
+echo $1
+
+if [ "$1" != "" ]; then
+    branch="$1"
+fi
+
+apt-get update
+apt-get install -y curl git
+
+#curl https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz > /tmp/go1.8.linux-amd64.tar.gz
+
+tar -C /usr/local -xzf /tmp/go1.8.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+mkdir -p /gopath
+export GOPATH=/gopath
+CLOUDINIT=github.com/0-complexity/cloud-init-server
+
+go get -v -d $CLOUDINIT
+
+cd $GOPATH/src/$CLOUDINIT
+
+git fetch origin "${branch}:${branch}" || true
+git checkout "${branch}" || true
+
+CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' .
+mkdir -p bin
+mv cloud-init-server bin/
+
+mkdir -p /tmp/archives/
+tar -czf "/tmp/archives/cloud-init-server-${branch}.tar.gz" -C $GOPATH/src/$CLOUDINIT bin
