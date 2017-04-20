ee9890bbc84e8d0f7e199ecccf86bb8f9953e271
Sarah Boctor <boctor.sarah@gmail.com>
2017-04-20 16:33:24 +0200

    Fix node reboot blueprint

--- api/node/node_api_RebootNode.go
+++ api/node/node_api_RebootNode.go
@@ -17,26 +17,16 @@ func (api NodeAPI) RebootNode(w http.ResponseWriter, r *http.Request) {
 	blueprint := map[string]interface{}{
 		"actions": []tools.ActionBlock{{
 			"action":  "reboot",
-			"actor":   "node",
+			"actor":   "node.g8os",
 			"service": nodeId,
 			"force":   true,
 		}},
 	}
 
-	run, err := tools.ExecuteBlueprint(api.AysRepo, "node", nodeId, "reboot", blueprint)
+	_, err := tools.ExecuteBlueprint(api.AysRepo, "node", nodeId, "reboot", blueprint)
 	if err != nil {
 		httpErr := err.(tools.HTTPError)
 		tools.WriteError(w, httpErr.Resp.StatusCode, httpErr)
-		return
-	}
-
-	if err = tools.WaitRunDone(run.Key, api.AysRepo); err != nil {
-		httpErr, ok := err.(tools.HTTPError)
-		if ok {
-			tools.WriteError(w, httpErr.Resp.StatusCode, httpErr)
-		} else {
-			tools.WriteError(w, http.StatusInternalServerError, err)
-		}
 		return
 	}
 
