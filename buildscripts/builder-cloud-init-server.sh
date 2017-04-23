fecead9566fd7d20b0f882341d328a1ede2ddf3d
Jo De Boeck <deboeck.jo@gmail.com>
2017-04-23 10:54:17 +0200

    Update cloud-init-server build

--- buildscripts/builder-cloud-init-server.sh
+++ buildscripts/builder-cloud-init-server.sh
@@ -11,7 +11,7 @@ fi
 apt-get update
 apt-get install -y curl git
 
-#curl https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz > /tmp/go1.8.linux-amd64.tar.gz
+curl https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz > /tmp/go1.8.linux-amd64.tar.gz
 
 tar -C /usr/local -xzf /tmp/go1.8.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
@@ -26,9 +26,9 @@ cd $GOPATH/src/$CLOUDINIT
 git fetch origin "${branch}:${branch}" || true
 git checkout "${branch}" || true
 
-CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' .
-mkdir -p bin
-mv cloud-init-server bin/
+mkdir -p build/bin
+mkdir -p build/etc/cloud-init
+CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' -o build/bin/cloud-init-server .
 
 mkdir -p /tmp/archives/
-tar -czf "/tmp/archives/cloud-init-server-${branch}.tar.gz" -C $GOPATH/src/$CLOUDINIT bin
+tar -czf "/tmp/archives/cloud-init-server-${branch}.tar.gz" -C $GOPATH/src/$CLOUDINIT/build .
