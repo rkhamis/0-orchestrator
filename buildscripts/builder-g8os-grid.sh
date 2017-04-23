ff52b1cd7d93f7048214e7428a663f62113c3b36
Christophe de Carvalho <zaibon@users.noreply.github.com>
2017-04-23 08:43:51 +0400

    add missing lib in grid flist build script (#108)

--- buildscripts/builder-g8os-grid.sh
+++ buildscripts/builder-g8os-grid.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+branch="master"
+echo $1
+
+if [ "$1" != "" ]; then
+    branch="$1"
+fi
+
 if ! grep 'xenial universe' /etc/apt/sources.list; then
     echo "deb http://archive.ubuntu.com/ubuntu xenial universe" >> /etc/apt/sources.list
 fi
@@ -8,12 +15,21 @@ fi
 apt-get update
 apt-get install -y git curl net-tools
 
+mkdir -p /root/.ssh
+touch /root/.ssh/known_hosts
+
+# install jumpscale
 export JSBRANCH="8.2.0"
 cd /tmp
 curl -k https://raw.githubusercontent.com/Jumpscale/jumpscale_core8/$JSBRANCH/install/install.sh?$RANDOM > install.sh
 bash install.sh
 
+# install grid actor template
+mkdir -p /opt/code/github/
+git clone -b "${branch}" https://github.com/g8os/grid.git /opt/code/github/g8os/grid
+
 pip3 install g8core
+pip3 install zerotier
 
 js 'x = j.tools.cuisine.local; x.apps.atyourservice.install()'
 js 'x = j.tools.cuisine.local; x.apps.caddy.build(); x.apps.caddy.install()'
@@ -21,4 +37,3 @@ js 'x = j.tools.cuisine.local; x.apps.caddy.build(); x.apps.caddy.install()'
 cd /
 mkdir /tmp/archives
 tar -czf /tmp/archives/grid.tar.gz --exclude tmp/archives --exclude sys --exclude dev --exclude proc *
-
