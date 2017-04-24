52c079ffc494ce126fd1490f5f129f688acae0c1
Sarah Boctor <boctor.sarah@gmail.com>
2017-04-24 14:52:10 +0200

    Refactor kill process api: - Refactor KillNodeProcess api to try to sigterm the process and if it fails, sigkill it. - Refactor KillContainerProcess api to try to sigterm the process and if it fails, sigkill it. - Move common code into tools.

--- api/node/node_api_KillContainerProcess.go
+++ api/node/node_api_KillContainerProcess.go
@@ -2,11 +2,7 @@ package node
 
 import (
 	"net/http"
-	"strconv"
 
-	"syscall"
-
-	client "github.com/g8os/go-client"
 	"github.com/g8os/grid/api/tools"
 	"github.com/gorilla/mux"
 )
@@ -22,17 +18,5 @@ func (api NodeAPI) KillContainerProcess(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	pID, err := strconv.ParseUint(vars["processid"], 10, 64)
-	if err != nil {
-		tools.WriteError(w, http.StatusInternalServerError, err)
-		return
-	}
-
-	processID := client.ProcessId(pID)
-	core := client.Core(cl)
-	if err := core.KillProcess(processID, syscall.SIGTERM); err != nil {
-		tools.WriteError(w, http.StatusInternalServerError, err)
-		return
-	}
-	w.WriteHeader(http.StatusNoContent)
+	tools.KillProcess(vars["processid"], cl, w)
 }
