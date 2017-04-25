bded68a71468116a3388007da3621408a0a7af16
Christophe de Carvalho <zaibon@users.noreply.github.com>
2017-04-25 16:31:15 +0400

    refactor bootstrap action (#124)
    retry to connect to g8os multiple times
    move authorize code in it's own function

--- templates/bootstrap.g8os/actions.py
+++ templates/bootstrap.g8os/actions.py
@@ -8,8 +8,6 @@ def is_valid_nic(nic):
 
 def bootstrap(job):
     from zerotier import client
-    from redis import ConnectionError
-    import time
 
     service = job.service
     token = service.model.data.zerotierToken
@@ -22,73 +20,85 @@ def bootstrap(job):
     members = resp.json()
 
     for member in members:
-        if not member['online'] or member['config']['authorized']:
-            continue
+        try_authorize(service, job.logger, netid, member, zerotier)
 
-        # authorized new member
-        job.logger.info("authorize new member {}".format(member['nodeId']))
-        member['config']['authorized'] = True
-        zerotier.network.updateMember(member, member['nodeId'], netid)
 
-        # get assigned ip of this member
+def try_authorize(service, logger, netid, member, zerotier):
+    from redis import ConnectionError
+    import time
+
+    if not member['online'] or member['config']['authorized']:
+        return
+
+    # authorized new member
+    logger.info("authorize new member {}".format(member['nodeId']))
+    member['config']['authorized'] = True
+    zerotier.network.updateMember(member, member['nodeId'], netid)
+
+    # get assigned ip of this member
+    resp = zerotier.network.getMember(member['nodeId'], netid)
+    member = resp.json()
+    while len(member['config']['ipAssignments']) <= 0:
+        time.sleep(1)
         resp = zerotier.network.getMember(member['nodeId'], netid)
         member = resp.json()
-        while len(member['config']['ipAssignments']) <= 0:
-            time.sleep(1)
-            resp = zerotier.network.getMember(member['nodeId'], netid)
-            member = resp.json()
-        zerotier_ip = member['config']['ipAssignments'][0]
+    zerotier_ip = member['config']['ipAssignments'][0]
 
-        # test if we can connect to the new member
-        job.logger.info("connection to g8os with IP: {}".format(zerotier_ip))
-        g8 = j.clients.g8core.get(zerotier_ip)
-        g8.timeout = 10
+    # test if we can connect to the new member
+    g8 = j.clients.g8core.get(zerotier_ip)
+    g8.timeout = 10
+    for attempt in range(5):
         try:
+            logger.info("connection to g8os with IP: {}".format(zerotier_ip))
             g8.ping()
+            break
         except:
-            # can't connect, unauthorize member
-            member['config']['authorized'] = False
-            zerotier.network.updateMember(member, member['nodeId'], netid)
-
-        # read mac Addr of g8os
-        mac = None
-        try:
-            for nic in filter(is_valid_nic, g8.info.nic()):
-                # get mac address and ip of the management interface
-                if len(nic['addrs']) > 0 and nic['addrs'][0]['addr'] != '':
-                    mac = nic['hardwareaddr']
-                    break
-        except ConnectionError:
-            job.logger.error("can't connect to g8os at {}".format(zerotier_ip))
             continue
+    else:
+        logger.info("can't connect, unauthorize member IP: {}".format(zerotier_ip))
+        member['config']['authorized'] = False
+        zerotier.network.updateMember(member, member['nodeId'], netid)
+        return
 
-        if mac is None:
-            job.logger.error("can't find mac address of the zerotier member ({})".format(member['physicalAddress']))
-            continue
+    # read mac Addr of g8os
+    mac = None
+    try:
+        for nic in filter(is_valid_nic, g8.info.nic()):
+            # get mac address and ip of the management interface
+            if len(nic['addrs']) > 0 and nic['addrs'][0]['addr'] != '':
+                mac = nic['hardwareaddr']
+                break
+    except ConnectionError:
+        logger.error("can't connect to g8os at {}".format(zerotier_ip))
+        return
 
-        # create node.g8os service
-        mac = mac.replace(':', '')
-        try:
-            node = service.aysrepo.serviceGet(role='node', instance=mac)
-            job.logger.info("service for node {} already exists, updating model".format(mac))
-            # mac sure the service has the correct ip in his model.
-            # it could happend that a node get a new ip after a reboot
-            node.model.data.redisAddr = zerotier_ip
-            node.model.data.status = 'running'
+    if mac is None:
+        logger.error("can't find mac address of the zerotier member ({})".format(member['physicalAddress']))
+        return
 
-        except j.exceptions.NotFound:
-            # create and install the node.g8os service
-            node_actor = job.service.aysrepo.actorGet('node.g8os')
-            networks = [n.name for n in service.producers.get('network', [])]
+    # create node.g8os service
+    mac = mac.replace(':', '')
+    try:
+        node = service.aysrepo.serviceGet(role='node', instance=mac)
+        logger.info("service for node {} already exists, updating model".format(mac))
+        # mac sure the service has the correct ip in his model.
+        # it could happend that a node get a new ip after a reboot
+        node.model.data.redisAddr = zerotier_ip
+        node.model.data.status = 'running'
 
-            node_args = {
-                'id': mac,
-                'status': 'running',
-                'networks': networks,
-                'redisAddr': zerotier_ip,
-            }
-            job.logger.info("create node.g8os service {}".format(mac))
-            node = node_actor.serviceCreate(instance=mac, args=node_args)
+    except j.exceptions.NotFound:
+        # create and install the node.g8os service
+        node_actor = service.aysrepo.actorGet('node.g8os')
+        networks = [n.name for n in service.producers.get('network', [])]
 
-            job.logger.info("install node.g8os service {}".format(mac))
-            j.tools.async.wrappers.sync(node.executeAction('install'))
+        node_args = {
+            'id': mac,
+            'status': 'running',
+            'networks': networks,
+            'redisAddr': zerotier_ip,
+        }
+        logger.info("create node.g8os service {}".format(mac))
+        node = node_actor.serviceCreate(instance=mac, args=node_args)
+
+        logger.info("install node.g8os service {}".format(mac))
+        j.tools.async.wrappers.sync(node.executeAction('install'))
