80131566e0a0bc6db55907174b387408e6b7e39a
Jo De Boeck <deboeck.jo@gmail.com>
2017-04-25 14:17:43 +0200

    Use the IP of the internet providing interface (#123)
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- templates/network.g8os/actions.py
+++ templates/network.g8os/actions.py
@@ -1,3 +1,6 @@
+from JumpScale import j
+
+
 def combine(ip1, ip2, mask):
     """
     >>> combine('10.0.3.11', '192.168.1.10', 24)
@@ -7,13 +10,23 @@ def combine(ip1, ip2, mask):
     iip1 = netaddr.IPNetwork('{}/{}'.format(ip1, mask))
     iip2 = netaddr.IPNetwork('{}/{}'.format(ip2, mask))
     ires = iip1.network + int(iip2.ip & (~ int(iip2.netmask)))
-    return ires.format()
+    net = netaddr.IPNetwork(ires)
+    net.prefixlen = mask
+    return net
 
 
 def configure(job):
     """
     this method will be called from the node.g8os install action.
     """
+    import netaddr
+
+    def get_nic_ip(nics, name):
+        for nic in nics:
+            if nic['name'] == name:
+                for ip in nic['addrs']:
+                    return netaddr.IPNetwork(ip['addr'])
+                return
 
     # if 'node' not in job.model.args:
     #     raise ValueError("argument node not present in job argument")
@@ -21,24 +34,32 @@ def configure(job):
     node = job.service.aysrepo.serviceGet(role='node', instance=job.model.args['node_name'])
     job.logger.info("execute network configure on {}".format(node))
 
-    service = job.service
-
-    net, mask = service.model.data.cidr.split('/')
-    mgmtaddr = node.model.data.redisAddr
-
-    storageaddr = combine(net, mgmtaddr, int(mask))
-    vxaddr = combine('10.240.0.0', mgmtaddr, int(mask))
-
     node_client = j.clients.g8core.get(host=node.model.data.redisAddr,
                                        port=node.model.data.redisPort,
                                        password=node.model.data.redisPassword)
 
-    node_client.timeout = 120
+    service = job.service
+
+    network = netaddr.IPNetwork(service.model.data.cidr)
+    defaultgwdev = node_client.bash("ip route | grep default | awk '{print $5}'").get().stdout.strip()
     nics = node_client.info.nic()
+    mgmtaddr = None
+    if defaultgwdev:
+        ipgwdev = get_nic_ip(nics, defaultgwdev)
+        if ipgwdev:
+            mgmtaddr = str(ipgwdev.ip)
+    if not mgmtaddr:
+        mgmtaddr = node.model.data.redisAddr
+
+    storageaddr = combine(str(network.ip), mgmtaddr, network.prefixlen)
+    vxaddr = combine('10.240.0.0', mgmtaddr, network.prefixlen)
+
+    node_client.timeout = 120
     nics.sort(key=lambda nic: nic['speed'])
     interface = None
     for nic in nics:
-        if any(addr['addr'].startswith(mgmtaddr + '/') for addr in nic['addrs'] if 'addr' in addr):
+        # skip all interface that have an ipv4 address
+        if any(netaddr.IPNetwork(addr['addr']).version == 4 for addr in nic['addrs'] if 'addr' in addr):
             continue
         if nic['speed'] == 0:
             continue
@@ -55,6 +76,6 @@ def configure(job):
     container_client = node_client.container.client(ovs_container_id)
     container_client.json('ovs.bridge-add', {"bridge": "backplane"})
     container_client.json('ovs.port-add', {"bridge": "backplane", "port": interface, "vlan": 0})
-    node_client.system('ip address add {addr}/{mask} dev backplane'.format(addr=storageaddr, mask=mask))
+    node_client.system('ip address add {addr} dev backplane'.format(addr=storageaddr))
     container_client.json('ovs.vlan-ensure', {'master': 'backplane', 'vlan': service.model.data.vlanTag, 'name': 'vxbackend'})
-    node_client.system('ip address add {addr}/{mask} dev vxbackend'.format(addr=vxaddr, mask=mask))
+    node_client.system('ip address add {addr} dev vxbackend'.format(addr=vxaddr))
