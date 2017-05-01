1e7b1da5c9406d6121df324853945d9765224366
Jo De Boeck <deboeck.jo@gmail.com>
2017-05-01 13:09:25 +0300

    Fix vdisk list (#148)
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- api/vdisk/vdisk_api_ListVdisks.go
+++ api/vdisk/vdisk_api_ListVdisks.go
@@ -13,21 +13,24 @@ import (
 // Get vdisk information
 func (api VdisksAPI) ListVdisks(w http.ResponseWriter, r *http.Request) {
 	vdiskID := mux.Vars(r)["vdiskid"]
+	queryParams := map[string]interface{}{
+		"fields": "storageCluster,type",
+	}
 
-	services, resp, err := api.AysAPI.Ays.ListServicesByRole("vdisk", api.AysRepo, nil, nil)
+	services, resp, err := api.AysAPI.Ays.ListServicesByRole("vdisk", api.AysRepo, nil, queryParams)
 	if !tools.HandleAYSResponse(err, resp, w, fmt.Sprintf("Listing vdisk services %s", vdiskID)) {
 		return
 	}
 
 	var respBody = make([]VdiskListItem, len(services))
-	for _, service := range services {
+	for idx, service := range services {
 		var vdiskInfo VdiskListItem
 		if err := json.Unmarshal(service.Data, &vdiskInfo); err != nil {
 			tools.WriteError(w, http.StatusInternalServerError, err)
 			return
 		}
 		vdiskInfo.ID = service.Name
-		respBody = append(respBody, vdiskInfo)
+		respBody[idx] = vdiskInfo
 	}
 
 	w.Header().Set("Content-Type", "application/json")
