6339f5a18b1e7dd98905c73d984b9250434c36f1
Jo De Boeck <deboeck.jo@gmail.com>
2017-05-05 13:58:02 +0200

    Add buildscript for openvswitch
    Do some refactor for ensuring go and lddcopy
    Also add lib to copying ubuntu packages
    
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- buildscripts/builder-grid-api.sh
+++ buildscripts/builder-grid-api.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+source $(dirname $0)/tools.sh
+ensure_go
 
 branch="master"
 echo $1
@@ -10,12 +12,6 @@ fi
 
 apt-get update
 apt-get install -y curl git
-
-curl https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz > /tmp/go1.8.linux-amd64.tar.gz
-tar -C /usr/local -xzf /tmp/go1.8.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-mkdir -p /gopath
-export GOPATH=/gopath
 
 git clone -b "${branch}" https://github.com/g8os/grid.git $GOPATH/src/github.com/g8os/grid
 
