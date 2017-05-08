471a1d87d7d45f66e88f2e89b8cbe9a9ba83cf6c
Jo De Boeck <deboeck.jo@gmail.com>
2017-05-08 12:50:17 +0300

    Make sure we use public interface for storage on packet.net (#181)
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

---
+++ templates/network.publicstorage/actions.py
@@ -0,0 +1,19 @@
+from JumpScale import j
+
+
+def configure(job):
+    """
+    For packet.net we just rename the public interface to storage so the rest of the config deals with it
+    this method will be called from the node.g8os install action.
+    """
+    from JumpScale.sal.g8os.Node import Node
+
+    nodeservice = job.service.aysrepo.serviceGet(role='node', instance=job.model.args['node_name'])
+    node = Node.from_ays(nodeservice)
+    node.client.bash("""
+    pubint=$(ip route | grep default | awk '{print $5}')
+    ip link set dev $pubint down
+    ip link set dev $pubint name storage
+    ip link set dev storage up
+    udhcpc -i storage -s /usr/share/udhcp/simple.script -q
+    """).get()
