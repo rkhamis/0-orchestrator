5f94f5adccb915c8974e67826f1c0ee4562ec982
Jo De Boeck <deboeck.jo@gmail.com>
2017-05-23 15:09:10 +0200

    Add support for multi private networks
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- templates/firewall/actions.py
+++ templates/firewall/actions.py
@@ -1,21 +1,21 @@
 def install(job):
     import ipaddress
     from JumpScale.sal.g8os.Container import Container
-    from JumpScale.sal.g8os.gateway.firewall import Firewall
+    from JumpScale.sal.g8os.gateway.firewall import Firewall, Network
     container = Container.from_ays(job.service.parent)
     gateway = job.service.parent.consumers['gateway'][0]
     gwdata = gateway.model.data.to_dict()
     portforwards = gwdata.get('portforwards', [])
     # lets assume the public ip is the ip of the nic which has a gateway configured
-    publicip = None
-    privatenetwork = None
+    publicnetwork = None
+    privatenetworks = []
     for nic in gateway.model.data.nics:
         if nic.config:
             if nic.config.gateway:
-                publicip = str(ipaddress.IPv4Interface(nic.config.cidr).ip)
+                publicnetwork = Network(nic.name, nic.config.cidr)
             else:
-                privatenetwork = str(ipaddress.IPv4Interface(nic.config.cidr).network)
-    firewall = Firewall(container, publicip, privatenetwork, portforwards)
+                privatenetworks.append(Network(nic.name, nic.config.cidr))
+    firewall = Firewall(container, publicnetwork, privatenetworks, portforwards)
     firewall.apply_rules()
 
 
