0d294ceab6137b6c7cfcc58f8e2013a23eb8956b
Jo De Boeck <deboeck.jo@gmail.com>
2017-05-25 09:41:21 +0300

    Fix for ndbserver stop (#278)
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- templates/nbdserver/actions.py
+++ templates/nbdserver/actions.py
@@ -108,10 +108,10 @@ def stop(job):
     import time
     service = job.service
     container = get_container(service=service)
-    job = is_running(container)
-    if job:
-        job.logger.info("killing job {}".format(job['cmd']['arguments']['name']))
-        container.client.job.kill(job['cmd']['id'])
+    nbdjob = is_running(container)
+    if nbdjob:
+        job.logger.info("killing job {}".format(nbdjob['cmd']['arguments']['name']))
+        container.client.job.kill(nbdjob['cmd']['id'])
 
         job.logger.info("wait for nbdserver to stop")
         for i in range(60):
@@ -119,4 +119,4 @@ def stop(job):
             if is_running(container):
                 continue
             return
-        raise j.exceptions.RuntimeError("ardb-server didn't stopped")
+        raise j.exceptions.RuntimeError("nbdserver didn't stopped")
