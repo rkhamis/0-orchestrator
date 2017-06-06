af5cc20dffa47b6cbd695a8c680c9ca856e3fc90
Khaled Karam <khaledkbadr@outlook.com>
2017-06-06 11:05:06 +0300

    Fix install-orchestrator (#373)

--- scripts/install-orchestrator.sh
+++ scripts/install-orchestrator.sh
@@ -34,6 +34,11 @@ BRANCH=$1
 ZEROTIERNWID=$2
 ZEROTIERTOKEN=$3
 
+CODEDIR="/root/gig/code"
+if [ "$GIGDIR" != "" ]; then
+    CODEDIR="$GIGDIR/code"
+fi
+
 echo "[+] Configuring zerotier"
 mkdir -p /etc/my_init.d > ${logfile} 2>&1
 ztinit="/etc/my_init.d/10_zerotier.sh"
@@ -43,36 +48,36 @@ echo 'zerotier-one -d' >> ${ztinit}
 echo 'while ! zerotier-cli info > /dev/null 2>&1; do sleep 0.1; done' >> ${ztinit}
 echo "[ $ZEROTIERNWID != \"\" ] && zerotier-cli join $ZEROTIERNWID" >> ${ztinit}
 
-chmod +x ${ztinit} > ${logfile} 2>&1
-bash $ztinit > ${logfile} 2>&1
+chmod +x ${ztinit} >> ${logfile} 2>&1
+bash $ztinit >> ${logfile} 2>&1
 
 echo "[+] Installing orchestrator dependencies"
-pip3 install -U "git+https://github.com/zero-os/0-core.git@${BRANCH}#subdirectory=client/py-client" > ${logfile} 2>&1
-pip3 install -U "git+https://github.com/zero-os/0-orchestrator.git@${BRANCH}#subdirectory=pyclient" > ${logfile} 2>&1
-pip3 install -U zerotier > ${logfile} 2>&1
-python3 -c "from js9 import j; j.tools.prefab.local.development.golang.install()" > ${logfile} 2>&1
-mkdir -p /usr/local/go > ${logfile} 2>&1
+pip3 install -U "git+https://github.com/zero-os/0-core.git@${BRANCH}#subdirectory=client/py-client" >> ${logfile} 2>&1
+pip3 install -U "git+https://github.com/zero-os/0-orchestrator.git@${BRANCH}#subdirectory=pyclient" >> ${logfile} 2>&1
+pip3 install -U zerotier >> ${logfile} 2>&1
+python3 -c "from js9 import j; j.tools.prefab.local.development.golang.install()" >> ${logfile} 2>&1
+mkdir -p /usr/local/go >> ${logfile} 2>&1
 
 echo "[+] Updating AYS orchestrator server"
-mkdir -p ${GIGDIR}/github > ${logfile} 2>&1
-pushd ${GIGDIR}/github
-mkdir -p zero-os > ${logfile} 2>&1
+mkdir -p $CODEDIR/github >> ${logfile} 2>&1
+pushd $CODEDIR/github
+mkdir -p zero-os >> ${logfile} 2>&1
 pushd zero-os
 
 if [ ! -d "0-orchestrator" ]; then
-    git clone https://github.com/zero-os/0-orchestrator.git > ${logfile} 2>&1
+    git clone https://github.com/zero-os/0-orchestrator.git >> ${logfile} 2>&1
 fi
 pushd 0-orchestrator
 git pull
-git checkout ${BRANCH} > ${logfile} 2>&1
+git checkout ${BRANCH} >> ${logfile} 2>&1
 popd
 
 if [ ! -d "0-core" ]; then
-    git clone https://github.com/zero-os/0-core.git > ${logfile} 2>&1
+    git clone https://github.com/zero-os/0-core.git >> ${logfile} 2>&1
 fi
 pushd 0-core
 git pull
-git checkout ${BRANCH} > ${logfile} 2>&1
+git checkout ${BRANCH} >> ${logfile} 2>&1
 popd
 
 echo "[+] Start AtYourService server"
@@ -81,25 +86,25 @@ aysinit="/etc/my_init.d/10_ays.sh"
 echo '#!/bin/bash -x' > ${aysinit}
 echo 'ays start > /dev/null 2>&1' >> ${aysinit}
 
-chmod +x ${aysinit} > ${logfile} 2>&1
-bash $aysinit > ${logfile} 2>&1
+chmod +x ${aysinit} >> ${logfile} 2>&1
+bash $aysinit >> ${logfile} 2>&1
 
 echo "[+] Building orchestrator api server"
-mkdir -p /opt/go/proj/src/github.com > ${logfile} 2>&1
+mkdir -p /opt/go/proj/src/github.com >> ${logfile} 2>&1
 if [ ! -d /opt/go/proj/src/github.com/zero-os ]; then
-    ln -sf /opt/code/github/zero-os /opt/go/proj/src/github.com/zero-os > ${logfile} 2>&1
+    ln -sf ${CODEDIR}/github/zero-os /opt/go/proj/src/github.com/zero-os >> ${logfile} 2>&1
 fi
 cd /opt/go/proj/src/github.com/zero-os/0-orchestrator/api
-GOPATH=/opt/go/proj GOROOT=/opt/go/root/ /opt/go/root/bin/go get -d ./... > ${logfile} 2>&1
-GOPATH=/opt/go/proj GOROOT=/opt/go/root/ /opt/go/root/bin/go build -o /usr/local/bin/orchestratorapiserver > ${logfile} 2>&1
+GOPATH=/opt/go/proj GOROOT=/opt/go/root/ /opt/go/root/bin/go get -d ./... >> ${logfile} 2>&1
+GOPATH=/opt/go/proj GOROOT=/opt/go/root/ /opt/go/root/bin/go build -o /usr/local/bin/orchestratorapiserver >> ${logfile} 2>&1
 if [ ! -d /optvar/cockpit_repos/orchestrator-server ]; then
-    ays repo create -n orchestrator-server -g js9 > ${logfile} 2>&1
+    ays repo create -n orchestrator-server -g js9 >> ${logfile} 2>&1
 fi
 
 echo "[+] Starting orchestrator api server"
 orchinit="/etc/my_init.d/11_orchestrator.sh"
 ZEROTIERIP=`ip -4 addr show zt0 | grep -oP 'inet\s\d+(\.\d+){3}' | sed 's/inet //' | tr -d '\n\r'`
-if [ $ZEROTIERIP == "" ]; then
+if [ "$ZEROTIERIP" == "" ]; then
     echo "zerotier doesn't have an ip. make sure you have authorize this docker in your netowrk"
     exit 1
 fi
@@ -111,13 +116,13 @@ echo 'tmux new-session -d -s main -n 1 || true' >> ${orchinit}
 echo 'tmux new-window -t main -n orchestrator' >> ${orchinit}
 echo 'tmux send-key -t orchestrator.0 "$cmd" ENTER' >> ${orchinit}
 
-chmod +x ${orchinit} > ${logfile} 2>&1
-bash $orchinit > ${logfile} 2>&1
+chmod +x ${orchinit} >> ${logfile} 2>&1
+bash $orchinit >> ${logfile} 2>&1
 
 echo "[+] Deploying bootstrap service"
 echo -e "bootstrap.g8os__grid1:\n  zerotierNetID: '"${ZEROTIERNWID}"'\n  zerotierToken: '"${ZEROTIERTOKEN}"'\n\nactions:\n  - action: install\n" > /optvar/cockpit_repos/orchestrator-server/blueprints/bootstrap.bp
-cd /optvar/cockpit_repos/orchestrator-server; ays blueprint > ${logfile} 2>&1
-cd /optvar/cockpit_repos/orchestrator-server; ays run create --follow -y > ${logfile} 2>&1
+cd /optvar/cockpit_repos/orchestrator-server; ays blueprint >> ${logfile} 2>&1
+cd /optvar/cockpit_repos/orchestrator-server; ays run create --follow -y >> ${logfile} 2>&1
 
 echo "Your ays server is ready to bootstrap nodes into your zerotier network."
 echo "Download your ipxe boot iso image https://bootstrap.gig.tech/iso/${BRANCH}/${ZEROTIERNWID} and boot up your nodes!"
