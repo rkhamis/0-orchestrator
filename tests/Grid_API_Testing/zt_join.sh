b469ec163d51f082626001075d3bfa33b1aa6961
john-kheir <kheirj@codescalers.com>
2017-06-12 13:19:17 +0200

    Adding script for joining zerotier network

---
+++ tests/Grid_API_Testing/zt_join.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+curl -s https://install.zerotier.com/ | sudo bash
+zerotier-cli join ${ZT_NET_ID}
+memberid=$(zerotier-cli info | awk '{print $3}')
+sleep 5
+curl -H "Content-Type: application/json" -H "Authorization: Bearer ${ZT_TOKEN}" -X POST -d '{"config": {"authorized": true}}' https://my.zerotier.com/api/network/${ZT_NET_ID}/member/${memberid}
+sleep 10
+
