a604ced1ebbfa38664a892b1d6bde4c73ce027cd
john-kheir <kheirj@codescalers.com>
2017-06-12 13:51:36 +0200

    fix

--- tests/Grid_API_Testing/zt_join.sh
+++ tests/Grid_API_Testing/zt_join.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -s https://install.zerotier.com/ | sudo bash
-zerotier-cli join ${ZT_NET_ID}
+sudo zerotier-cli join ${ZT_NET_ID}
 memberid=$(zerotier-cli info | awk '{print $3}')
 sleep 5
 curl -H "Content-Type: application/json" -H "Authorization: Bearer ${ZT_TOKEN}" -X POST -d '{"config": {"authorized": true}}' https://my.zerotier.com/api/network/${ZT_NET_ID}/member/${memberid}
