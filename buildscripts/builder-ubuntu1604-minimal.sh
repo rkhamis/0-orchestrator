f4364599a71c8d71755c7b3256ddbed8405e1e4f
Maxime Daniel <maxux.unix@gmail.com>
2017-06-20 11:47:18 +0200

    Add ssh by default to ubuntu and clean ubuntu-minimal (#505)
    * Add ssh by default to ubuntu and clean ubuntu-minimal
    
    * Add ca-certificates and curl to ubuntu default flist

--- buildscripts/builder-ubuntu1604-minimal.sh
+++ buildscripts/builder-ubuntu1604-minimal.sh
@@ -8,8 +8,9 @@ apt-get install -y debootstrap
 
 mkdir /mnt/ubuntu
 debootstrap --arch amd64 xenial /mnt/ubuntu
+sed -i "s/main/main restricted universe multiverse/" /mnt/ubuntu/etc/apt/sources.list
 
 cd /mnt/ubuntu
 
-mkdir /tmp/archives
-tar -czf /tmp/archives/ubuntu1604.tar.gz *
+mkdir -p /tmp/archives
+tar -czf /tmp/archives/ubuntu1604-minimal.tar.gz *
--- buildscripts/builder-ubuntu1604.sh
+++ buildscripts/builder-ubuntu1604.sh
@@ -7,10 +7,14 @@ apt-get update
 apt-get install -y debootstrap
 
 mkdir /mnt/ubuntu
-debootstrap --arch amd64 xenial /mnt/ubuntu
-sed -i "s/main/main multiverse universe/" /mnt/ubuntu/etc/apt/sources.list
+debootstrap --include openssh-server,curl,ca-certificates --arch amd64 xenial /mnt/ubuntu
+
+sed -i "s/main/main restricted universe multiverse/" /mnt/ubuntu/etc/apt/sources.list
+
+rm -rf /mnt/ubuntu/etc/ssh/ssh_host_*
+mkdir -p /mnt/ubuntu/root/.ssh
 
 cd /mnt/ubuntu
 
-mkdir /tmp/archives
+mkdir -p /tmp/archives
 tar -czf /tmp/archives/ubuntu1604.tar.gz *
