5c0383eef5f302967271d61634da2240eb452bbd
serboctor <boctor.sarah@gmail.com>
2017-06-20 10:53:49 +0200

    fix data reference in zerotier install. (#504)
    * fix data reference in zerotier install.
    
    * Wait until status is OK for zerotier with a supplied token.

--- templates/zerotier/actions.py
+++ templates/zerotier/actions.py
@@ -14,58 +14,61 @@ def install(job):
     import time
     from zerotier import client as ztclient
 
-    data = job.service.model.data
     client = _get_client(job)
-    client.zerotier.join(data.nwid)
+    client.zerotier.join(job.service.model.data.nwid)
 
     def get_member():
         start = time.time()
         while start + 60 > time.time():
-            resp = zerotier.network.getMember(address, data.nwid)
+            resp = zerotier.network.getMember(address, job.service.model.data.nwid)
             if resp.content:
                 return resp.json()
             time.sleep(0.5)
         raise j.exceptions.RuntimeError('Could not find member on zerotier network')
 
-    if data.token:
+    token = job.service.model.data.token
+    if token:
         address = client.zerotier.info()['address']
         zerotier = ztclient.Client()
-        zerotier.set_auth_header('bearer {}'.format(data.token))
+        zerotier.set_auth_header('bearer {}'.format(token))
 
         member = get_member()
         if not member['config']['authorized']:
             # authorized new member
-            job.logger.info("authorize new member {} to network {}".format(member['nodeId'], data.nwid))
+            job.logger.info("authorize new member {} to network {}".format(
+                member['nodeId'], job.service.model.data.nwid))
             member['config']['authorized'] = True
-            zerotier.network.updateMember(member, member['nodeId'], data.nwid)
+            zerotier.network.updateMember(member, member['nodeId'], job.service.model.data.nwid)
 
     while True:
         net = _get_network(job)
-        if net['status'] in ['OK', 'ACCESS_DENIED']:
+
+        if (token and net['status'] == 'OK') or (not token and net['status'] in ['OK', 'ACCESS_DENIED']):
             break
         time.sleep(1)
 
-    data.allowDefault = net['allowDefault']
-    data.allowGlobal = net['allowGlobal']
-    data.allowManaged = net['allowManaged']
-    data.assignedAddresses = net['assignedAddresses']
-    data.bridge = net['bridge']
-    data.broadcastEnabled = net['broadcastEnabled']
-    data.dhcp = net['dhcp']
-    data.mac = net['mac']
-    data.mtu = net['mtu']
-    data.name = net['name']
-    data.netconfRevision = net['netconfRevision']
-    data.portDeviceName = net['portDeviceName']
-    data.portError = net['portError']
+    job.service.model.data.allowDefault = net['allowDefault']
+    job.service.model.data.allowGlobal = net['allowGlobal']
+    job.service.model.data.allowManaged = net['allowManaged']
+    job.service.model.data.assignedAddresses = net['assignedAddresses']
+    job.service.model.data.bridge = net['bridge']
+    job.service.model.data.broadcastEnabled = net['broadcastEnabled']
+    job.service.model.data.dhcp = net['dhcp']
+    job.service.model.data.mac = net['mac']
+    job.service.model.data.mtu = net['mtu']
+    job.service.model.data.name = net['name']
+    job.service.model.data.netconfRevision = net['netconfRevision']
+    job.service.model.data.portDeviceName = net['portDeviceName']
+    job.service.model.data.portError = net['portError']
 
     for route in net['routes']:
         if route['via'] is None:
             route['via'] = ''
 
-    data.routes = net['routes']
-    data.status = net['status']
-    data.type = net['type'].lower()
+    job.service.model.data.routes = net['routes']
+    job.service.model.data.status = net['status']
+    job.service.model.data.type = net['type'].lower()
+    job.service.saveAll()
 
 
 def delete(job):
