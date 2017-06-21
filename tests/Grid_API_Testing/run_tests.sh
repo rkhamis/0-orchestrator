0fc17a882a36724e3d0760e7ce0f86785714a89c
john-kheir <kheirj@codescalers.com>
2017-06-21 14:29:48 +0200

    fix pathes

--- tests/Grid_API_Testing/run_tests.sh
+++ tests/Grid_API_Testing/run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ "$TRAVIS_EVENT_TYPE" == "cron" ]
 then
+   cd tests/Grid_API_Testing/
    nosetests-3.4 -v -s api_testing/testcases/basic_tests/test01_nodeid_apis.py:TestNodeidAPI.test001_list_nodes --tc-file=api_testing/config.ini
 else
    echo "Not a cron job" 
