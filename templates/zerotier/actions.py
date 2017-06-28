0c9c7cc36b9f81a70ce8371be226d8c29bc30a1f
serboctor <boctor.sarah@gmail.com>
2017-06-28 11:15:19 +0200

    Implement zerotier monitoring (#542)
    * Implement zerotier monitoring.
    
    * Change the zerotier monitoring interval.

--- templates/zerotier/actions.py
+++ templates/zerotier/actions.py
@@ -1,20 +1,45 @@
-def _get_client(job):
+def _get_client(job, token):
     from zeroos.orchestrator.sal.Node import Node
-    return Node.from_ays(job.service.parent, job.context['token']).client
+    return Node.from_ays(job.service.parent, token).client
 
 
-def _get_network(job):
-    client = _get_client(job)
+def _get_network(job, token):
+    client = _get_client(job, token)
     for net in client.zerotier.list():
         if net['id'] == job.service.model.data.nwid:
             return net
+
+
+def _update_model(job, network):
+    job.service.model.data.allowDefault = network['allowDefault']
+    job.service.model.data.allowGlobal = network['allowGlobal']
+    job.service.model.data.allowManaged = network['allowManaged']
+    job.service.model.data.assignedAddresses = network['assignedAddresses']
+    job.service.model.data.bridge = network['bridge']
+    job.service.model.data.broadcastEnabled = network['broadcastEnabled']
+    job.service.model.data.dhcp = network['dhcp']
+    job.service.model.data.mac = network['mac']
+    job.service.model.data.mtu = network['mtu']
+    job.service.model.data.name = network['name']
+    job.service.model.data.netconfRevision = network['netconfRevision']
+    job.service.model.data.portDeviceName = network['portDeviceName']
+    job.service.model.data.portError = network['portError']
+
+    for route in network['routes']:
+        if route['via'] is None:
+            route['via'] = ''
+
+    job.service.model.data.routes = network['routes']
+    job.service.model.data.status = network['status']
+    job.service.model.data.type = network['type'].lower()
+    job.service.saveAll()
 
 
 def install(job):
     import time
     from zerotier import client as ztclient
 
-    client = _get_client(job)
+    client = _get_client(job, job.context['token'])
     client.zerotier.join(job.service.model.data.nwid)
 
     def get_member():
@@ -41,37 +66,23 @@ def install(job):
             zerotier.network.updateMember(member, member['nodeId'], job.service.model.data.nwid)
 
     while True:
-        net = _get_network(job)
+        net = _get_network(job, job.context['token'])
 
         if (token and net['status'] == 'OK') or (not token and net['status'] in ['OK', 'ACCESS_DENIED']):
             break
         time.sleep(1)
 
-    job.service.model.data.allowDefault = net['allowDefault']
-    job.service.model.data.allowGlobal = net['allowGlobal']
-    job.service.model.data.allowManaged = net['allowManaged']
-    job.service.model.data.assignedAddresses = net['assignedAddresses']
-    job.service.model.data.bridge = net['bridge']
-    job.service.model.data.broadcastEnabled = net['broadcastEnabled']
-    job.service.model.data.dhcp = net['dhcp']
-    job.service.model.data.mac = net['mac']
-    job.service.model.data.mtu = net['mtu']
-    job.service.model.data.name = net['name']
-    job.service.model.data.netconfRevision = net['netconfRevision']
-    job.service.model.data.portDeviceName = net['portDeviceName']
-    job.service.model.data.portError = net['portError']
-
-    for route in net['routes']:
-        if route['via'] is None:
-            route['via'] = ''
-
-    job.service.model.data.routes = net['routes']
-    job.service.model.data.status = net['status']
-    job.service.model.data.type = net['type'].lower()
-    job.service.saveAll()
+    _update_model(job, net)
 
 
 def delete(job):
     service = job.service
-    client = _get_client(job)
+    client = _get_client(job, job.context['token'])
     client.zerotier.leave(service.model.data.nwid)
+
+
+def monitor(job):
+    from zeroos.orchestrator.configuration import get_jwt_token
+
+    if job.service.model.actionsState['install'] == 'ok':
+        _update_model(job, _get_network(job,  get_jwt_token(job.service.aysrepo)))
